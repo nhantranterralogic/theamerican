e81114255da5c1a2bf166b0c8673c7259a3217e2
viennguyen55 <nhan.tran@terralogic.com>
2016-12-28 11:25:59 +0700

    AMERICAN #comment setup region of files.

---
+++ custom.js
@@ -0,0 +1,78 @@
+//////////////////
+/// INIT VARIABLES ....
+////////////////
+
+
+/*giang.nguyen*/
+
+
+/*nhan.tran*/
+
+
+
+
+/* attack functions */
+/**
+ * When body is starting to render
+ * 1. Hide original/in-build content
+ * 2. Pre-populate data for rendering when body rendering is done
+ */
+Worldnow.EventMan.event('bodystart', function () {
+   /* giang.nguyen */
+
+   /*nhan.tran*/
+
+});
+/**
+ * col4 is ready to serve
+ */
+Worldnow.EventMan.event('WNCol4done', function () {
+   /* giang.nguyen */
+
+   /*nhan.tran*/
+   
+});
+
+/**
+ * Col23 is ready to serve
+ */
+Worldnow.EventMan.event('wncol23done', function () {
+   /* giang.nguyen */
+
+   /*nhan.tran*/
+   
+});
+
+/**
+ * Body is done
+ * 1. Show custom/populated data
+ */
+Worldnow.EventMan.event('bodydone', function () {
+    /* giang.nguyen */
+
+   /*nhan.tran*/
+   
+});
+window.onload = function () {
+    /* giang.nguyen */
+
+   /*nhan.tran*/
+   
+}
+
+/**
+ * Populate data when document is ready
+ */
+Worldnow.EventMan.event('documentready', function () {
+    /* giang.nguyen */
+
+   /*nhan.tran*/
+   
+});
+
+$(window).load(function () {
+ /* giang.nguyen */
+
+/*nhan.tran*/
+   
+});
